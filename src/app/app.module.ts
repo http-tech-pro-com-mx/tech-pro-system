2ce48ff4bcd54f3f2c160631549a0f5023dc1196
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2019-03-11 17:57:13 -0600

    Template dividido en componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoaderComponent } from './loader/loader.component';
+import { LoginComponent } from './login/login.component';
+import { NotFound404Component } from './not-found404/not-found404.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: NotFound404Component }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoaderComponent,
+    LoginComponent,
+    NotFound404Component,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
