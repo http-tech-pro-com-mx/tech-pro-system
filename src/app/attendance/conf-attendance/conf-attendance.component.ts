e3ad0566b82f6f98740336262f1fcdc662d31995
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2019-05-03 14:28:50 -0500

    Consulta de quincenas

--- src/app/attendance/conf-attendance/conf-attendance.component.ts
+++ src/app/attendance/conf-attendance/conf-attendance.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { ConfAttendanceService } from './conf-attendance.service';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Quincena } from 'src/app/models/quincena';
 
+
+declare var $: any;
+declare var toastr: any;
 @Component({
   selector: 'app-conf-attendance',
   templateUrl: './conf-attendance.component.html',
@@ -8,11 +15,63 @@ import { Component, OnInit } from '@angular/core';
 export class ConfAttendanceComponent implements OnInit {
 
   public section:string;
+  public loading: boolean;
+  public status_message: string;
+  public submitted: boolean;
+  public form: FormGroup;
+  public quincenas: Array<Quincena>;
 
-  constructor() { }
+  constructor(private service: ConfAttendanceService,
+    private fb: FormBuilder,
+    private auth: AuthService) { }
 
   ngOnInit() {
+    
+    this.loading = true;
     this.section = "CONFIGURACIÓN";
+    this.status_message = null;
+    this.submitted = false;
+    this.quincenas = [];
+
+    this.service.findAllQuincenas().subscribe(response => {
+      console.log('quincenas',response)
+      if (response.successful) {
+        this.quincenas = response.quincenas;
+        this.status_message = null;
+
+      } else {
+        toastr.error(response.message);
+        this.status_message = " " + response.message;
+      }
+
+      this.loading = false;
+
+    }, error => {
+      this.status_message = 'Error: ' + error.status;
+      toastr.error('Ocurrió un error al consultar! Error: ' + error.status);
+      this.loading = false;
+    });
+
+  }
+
+  ngAfterInitEffectForm(): void {
+
+    this.form = this.fb.group({
+      anio: new FormControl('', [Validators.required]),
+      mes: new FormControl('', [Validators.required]),
+      quincena: new FormControl('', [Validators.required]),
+      id_personal: new FormControl('', [Validators.required])
+    });
+
+    setTimeout(function () {
+      $.AdminBSB.select.activate();
+    }, 100);
+
+  }
+
+  consultaDiasHabiles(id_quincena: number, event): void{
+    event.preventDefault();
+    alert('Consulta para quincena: '+ id_quincena)
   }
 
 }
---
+++ src/app/models/quincena.ts
@@ -0,0 +1,16 @@
+import { Mes } from './mes';
+import { Anio } from './anio';
+
+export class Quincena{
+    constructor(
+        public id_quincena: number,
+        public id_mes: Mes,
+        public id_anio: Anio,
+        public numero_quincena: number,
+        public hora_entrada: string,
+        public hora_salida_comida: string,
+        public hora_entrada_comida: string, 
+        public hora_salida: string, 
+        public activo: boolean
+    ){}
+}
