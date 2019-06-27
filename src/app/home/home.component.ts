2b3ef3053a0a048c4e78e2578b95a7d8399804c0
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2019-06-27 13:43:34 -0500

    Menu restricciones

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
 import { Usuario } from '../models/usuario';
 import { Personal } from '../models/personal';
@@ -17,12 +18,30 @@ export class HomeComponent implements OnInit, AfterViewInit {
   public personal: Personal;
   public URL_IMAGEN: string = BASE_URL + '/api/usuarios/getImageProfile/';
   public url: string;
-
-  constructor(private router: Router) { }
+  public permissions: any = {
+    consulta_empleados: false,
+    consulta_quincena: false,
+    rpt_individual: false,
+    rpt_admin: false,
+    rpt_concentrado: false,
+    jst_create: false,
+    jft_empleados: false,
+    consulta_jst: false
+  };
+  constructor(private router: Router,
+    private auth: AuthService) {
+        this.permissions.consulta_empleados = this.auth.hasPermission('ROLE_CONSULTA_USUARIO');
+        this.permissions.consulta_quincena = this.auth.hasPermission('ROLE_CONSULTA_QUINCENA');
+        this.permissions.rpt_individual = this.auth.hasPermission('ROLE_RPT_INDIVIDUAL');
+        this.permissions.rpt_admin = this.auth.hasPermission('ROLE_RPT_ADMIN');
+        this.permissions.rpt_concentrado = this.auth.hasPermission('ROLE_RPT_CONCENTRADO');
+        this.permissions.jst_create = this.auth.hasPermission('ROLE_CREA_JST');
+        this.permissions.jft_empleados = this.auth.hasPermission('ROLE_CREA_JST_EMPLEADO');
+        this.permissions.consulta_jst = this.auth.hasPermission('ROLE_CONSULTA_JST');
+  }
 
   ngOnInit() {
     this.url = this.router.url;
-    
     let datos = JSON.parse(localStorage.getItem('data_user'));
     this.personal = new Personal(datos.id_personal, datos.nombre, datos.apellido_paterno, datos.apellido_materno, datos.genero, datos.nombre_foto, datos.email, datos.id_area, datos.id_perfil);
 
@@ -44,7 +63,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
         element_menu = ".menu-asistencias,.menu-asistencias-rpt";
       } else if (this.url == "/home/attendance/report-concentrated") {
         element_menu = ".menu-asistencias,.menu-asistencias-c";
-      }else if (this.url.includes("/home/attendance/justication/crear")) {
+      } else if (this.url.includes("/home/attendance/justication/crear")) {
         element_menu = ".menu-justicantes,.menu-justicantes-crear";
       } else if (this.url.includes("/home/attendance/justication/employees/crear")) {
         element_menu = ".menu-justicantes,.menu-justicantes-empl";
